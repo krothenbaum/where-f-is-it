0252f5733e36a63c45cd88f441a35d1a32ca668b
Kyle Rothenbaum <kyle@heywelcome.com>
2022-12-12 15:03:53 -0800

    WTFII-01 - add mantine ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
 import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
@@ -11,9 +12,28 @@ const MyApp: AppType<{ session: Session | null }> = ({
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: "light",
+        }}
+      >
+        <SessionProvider session={session}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </MantineProvider>
+    </>
   );
 };
 
